b9cd70009b86c14fd3c6d4829565437a68eb3e1e
John Doe <hamedhasan422421@gmail.com>
2023-06-16 12:26:49 +0300

    feat: Implement database connection and server setup with event handling
    - Connect to MongoDB using the provided DATABASE_URL with additional options for useNewUrlParser and useUnifiedTopology.
    - Start the server using the app instance and listen on the specified PORT.
    - Handle the SIGTERM signal to gracefully shut down the server.
    - Handle unhandled promise rejections and uncaught exceptions for proper error handling.
    - Log meaningful messages for successful database connection and server start.

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,35 @@
+import { Server } from 'http';
 import app from './app';
 import mongoose from 'mongoose';
 import { DATABASE_URL, PORT } from '../config';
 
+let server: Server;
+
 async function dataBase() {
   try {
     await mongoose.connect(DATABASE_URL as string);
-    console.log(`🛢 Database is connected successfully`);
+    console.log('🛢 Database is connected successfully');
 
-    const server = app.listen(PORT, () => {
-      console.log(`Application listening on port ${PORT}`);
+    server = app.listen(PORT, () => {
+      console.log(`Digital Cow Hut Backend on port ${PORT}`);
+    });
+
+    process.on('SIGTERM', () => {
+      console.log('Received SIGTERM. Shutting down gracefully...');
+      server.close(() => {
+        console.log('Server closed.');
+        process.exit(0);
+      });
+    });
+
+    process.on('unhandledRejection', (err) => {
+      console.error('Unhandled Promise Rejection:', err);
+      process.exit(1);
+    });
+
+    process.on('uncaughtException', (err) => {
+      console.error('Uncaught Exception:', err);
+      process.exit(1);
     });
   } catch (err) {
     console.log('Failed to connect to the database', err);
