d0d6465c8c71bb3b37fa52455fec23d08dc89fea
John Doe <hamedhasan422421@gmail.com>
2023-06-17 00:45:14 +0300

    Added createCow function to handle the API endpoint Validated the incoming cow data using createCowSchema Created a new cow document using the CowModel Populated the cow document with seller information Responded with a success message and the populated cow document on successful creation.

---
+++ src/models/cow/cowController.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from 'express';
+import { createCowSchema } from './cowValidation';
+import CowModel, { CowDocument } from './cowModel';
+import { APIError } from '../../utils/apiError';
+import { handleResponse } from '../../utils/responseHandler';
+
+export const createCow = async (req: Request, res: Response): Promise<void> => {
+    try {
+      const validatedData = createCowSchema.parse(req.body);
+      const cow = await CowModel.create(validatedData);
+      const populatedCow = await cow.populate('seller');
+      handleResponse(res, 201, 'Cow created successfully', populatedCow);
+    } catch (error) {
+      if (error instanceof APIError) {
+        handleResponse(res, error.statusCode, error.message);
+      } else {
+        handleResponse(res, 500, 'Internal Server Error');
+      }
+    }
+  };
+  
+
