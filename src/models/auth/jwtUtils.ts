3dd2055ae1f3631e33cd588652e9b3be43c3a301
John Doe <hamedhasan422421@gmail.com>
2023-06-23 23:15:14 +0300

    feat: Add token generation and verification functions
    Implement functions for generating access tokens, refresh tokens, and verifying tokens using JSON Web Tokens (JWT).
    
    The generateAccessToken function takes a payload and returns a signed access token using the secret key from the configuration file. The access token expires after a duration specified in the configuration.
    
    The generateRefreshToken function is similar to generateAccessToken but generates a refresh token instead.
    
    The verifyToken function takes a token as input and verifies its authenticity using the secret key from the configuration file.

---
+++ src/models/auth/jwtUtils.ts
@@ -0,0 +1,15 @@
+import jwt from 'jsonwebtoken';
+import { config } from '../../../config/config';
+
+
+export const generateAccessToken = (payload: any): string => {
+  return jwt.sign(payload, config.secretKey, { expiresIn: config.accessTokenExpiration });
+};
+
+export const generateRefreshToken = (payload: any): string => {
+  return jwt.sign(payload, config.secretKey, { expiresIn: config.refreshTokenExpiration });
+};
+
+export const verifyToken = (token: string): any => {
+  return jwt.verify(token, config.secretKey);
+};
