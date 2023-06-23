e5a0814c97427008efa705723f096d186461b0de
John Doe <hamedhasan422421@gmail.com>
2023-06-23 23:09:26 +0300

    feat: Add token authentication middleware
    This commit adds a token authentication middleware to the codebase. The authenticateToken function verifies the token provided in the request headers and sets the decoded user information on the request object. If the token is missing or invalid, it returns an appropriate error response.
    
    The AuthenticatedRequest interface extends the Express Request interface to include the user property, which will hold the decoded user information.
    
    This middleware can be used to protect routes that require authentication and ensure that only authorized users can access them.

---
+++ src/models/auth/authentication.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction } from 'express';
+import { verifyToken } from './jwtUtils';
+
+interface AuthenticatedRequest extends Request {
+  user?: any;
+}
+
+export const authenticateToken = (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+  const token = req.headers.authorization?.split(' ')[1];
+  if (!token) {
+    return res.status(401).json({ success: false, message: 'Authentication failed: Token missing' });
+  }
+
+  try {
+    const decoded = verifyToken(token);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(401).json({ success: false, message: 'Authentication failed: Invalid token' });
+  }
+};
