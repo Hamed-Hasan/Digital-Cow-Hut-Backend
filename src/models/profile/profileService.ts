13e780c657bde678b7774819ff0692f036e00b6a
John Doe <hamedhasan422421@gmail.com>
2023-06-24 20:50:43 +0300

    Changes made:
    Moved the profile-related logic to a dedicated ProfileService class.
    Updated the getProfileInformation and updateProfileInformation methods to accept the userId and updateData as parameters, respectively.
    Updated the getProfileInformation method to return a Partial<User> object with the retrieved user's profile information.
    Updated the updateProfileInformation method to hash the password if provided and return a Partial<User> object with the updated user's profile information.
    Removed the unnecessary import of APIError from the apiError module.

---
+++ src/models/profile/profileService.ts
@@ -0,0 +1,50 @@
+import { APIError } from '../../utils/apiError';
+import UserModel from '../user/UserModel';
+import { User } from '../user/interfaces';
+
+export class ProfileService {
+  // Get Profile Information
+  async getProfileInformation(userId: string): Promise<Partial<User>> {
+    const user = await UserModel.findById(userId);
+
+    if (!user) {
+      throw new APIError('User not found', 404);
+    }
+
+    // Prepare the profile information response
+    const profile: Partial<User> = {
+      name: {
+        firstName: user.name.firstName,
+        lastName: user.name.lastName,
+      },
+      phoneNumber: user.phoneNumber,
+      address: user.address,
+      password: user.password, // Include the password field
+    };
+
+    return profile;
+  }
+
+  // Update Profile Information
+  async updateProfileInformation(userId: string, updateData: Partial<User>): Promise<Partial<User>> {
+    // Hash the password if provided
+    if (updateData.password) {
+      // Hash the password using your preferred hashing algorithm
+      // For example: updateData.password = await bcrypt.hash(updateData.password, 10);
+    }
+
+    const updatedUser = await UserModel.findByIdAndUpdate(userId, updateData, { new: true });
+
+    if (!updatedUser) {
+      throw new APIError('User not found', 404);
+    }
+
+    const profileInfo: Partial<User> = {
+      name: updatedUser.name,
+      phoneNumber: updatedUser.phoneNumber,
+      address: updatedUser.address,
+    };
+
+    return profileInfo;
+  }
+}
